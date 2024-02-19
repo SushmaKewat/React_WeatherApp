529f413d18545cbb30b496941f2ed28213ac31a2
Sushma <sushmak1693@gmail.com>
2024-02-19 21:57:43 +0530

    added background blur to hover cards for readability & address line 2 in autocomplete result

--- src/components/Autocomplete.jsx
+++ src/components/Autocomplete.jsx
@@ -1,36 +1,35 @@
-import React from "react";
+import React from 'react';
 import {
-    GeoapifyGeocoderAutocomplete,
-    GeoapifyContext,
-} from "@geoapify/react-geocoder-autocomplete";
-import "@geoapify/geocoder-autocomplete/styles/minimal.css";
-import "prismjs/themes/prism-solarizedlight.css";
-import "../index.css";
+	GeoapifyGeocoderAutocomplete,
+	GeoapifyContext,
+} from '@geoapify/react-geocoder-autocomplete';
+import '@geoapify/geocoder-autocomplete/styles/minimal.css';
+import 'prismjs/themes/prism-solarizedlight.css';
+import '../index.css';
 
 const Autocomplete = ({ value, setCity }) => {
-    function onPlaceSelect(value) {
-        console.log(value);
-        const result =
-            value.properties.address_line1 ||
-            value.properties.city ||
-            value.properties.state ||
-            value.properties.county ||
-            value.properties.country;
-        if (result) setCity(result);
-    }
+	function onPlaceSelect(value) {
+		console.log(value);
+		const result =
+			value.properties.address_line1 ||
+			value.properties.address_line2 ||
+			value.properties.city ||
+			value.properties.state ||
+			value.properties.county ||
+			value.properties.country;
+		if (result) setCity(result);
+	}
 
-    return (
-        <GeoapifyContext apiKey={`${process.env.REACT_APP_GEOAPIFY_API_KEY}`}>
-            <GeoapifyGeocoderAutocomplete
-                skipIcons
-                value={value}
-                placeSelect={onPlaceSelect}
-                placeholder="enter a city, country ... "
-                allowNonVerifiedHouseNumber={true}
-                allowNonVerifiedStreet={true}
-            />
-        </GeoapifyContext>
-    );
+	return (
+		<GeoapifyContext apiKey={`${process.env.REACT_APP_GEOAPIFY_API_KEY}`}>
+			<GeoapifyGeocoderAutocomplete
+				skipIcons
+				value={value}
+				placeSelect={onPlaceSelect}
+				placeholder='enter a city, country ... '
+			/>
+		</GeoapifyContext>
+	);
 };
 
 export default Autocomplete;
--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,91 +1,93 @@
-import { DateTime } from "luxon";
+import { DateTime } from 'luxon';
+import { useState } from 'react';
 
 const API_KEY = `${process.env.REACT_APP_OPENWEATHER_API_KEY}`;
 const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
 
 const getWeatherData = async (infoType, searchParams) => {
-    const url = new URL(BASE_URL + "/" + infoType);
-    url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
-    return fetch(url).then((res) => res.json());
+	const url = new URL(BASE_URL + '/' + infoType);
+	url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
+	return fetch(url).then((res) => res.json());
 };
 
 const formatCurrentWeather = (data) => {
-    console.log(data);
-    const {
-        coord: { lat, lon },
-        main: { temp, feels_like, temp_min, temp_max, humidity, pressure },
-        name,
-        dt,
-        sys: { country, sunrise, sunset },
-        weather,
-        wind: { speed },
-    } = data;
+	console.log(data);
+	const {
+		coord: { lat, lon },
+		main: { temp, feels_like, temp_min, temp_max, humidity, pressure },
+		name,
+		dt,
+		sys: { country, sunrise, sunset },
+		weather,
+		wind: { speed },
+	} = data;
 
-    const { main: details, icon } = weather[0];
+	const { main: details, icon } = weather[0];
 
-    return {
-        lon,
-        lat,
-        temp,
-        feels_like,
-        temp_min,
-        temp_max,
-        humidity,
-        pressure,
-        name,
-        dt,
-        country,
-        sunrise,
-        sunset,
-        details,
-        icon,
-        speed,
-    };
+	return {
+		lon,
+		lat,
+		temp,
+		feels_like,
+		temp_min,
+		temp_max,
+		humidity,
+		pressure,
+		name,
+		dt,
+		country,
+		sunrise,
+		sunset,
+		details,
+		icon,
+		speed,
+	};
 };
 
 const formatForecastWeather = (data) => {
-    let { timezone, daily, hourly } = data;
+	let { timezone, daily, hourly } = data;
 
-    daily = daily.slice(1, 12).map((d) => {
-        return {
-            title: formatToLocalTime(d.dt, timezone, "ccc"),
-            temp: d.temp.day,
-            icon: d.weather[0].icon,
-            humidity: d.humidity,
-            description: d.weather[0].description,
-        };
-    });
-    hourly = hourly.slice(1, 12).map((d) => {
-        return {
-            title: formatToLocalTime(d.dt, timezone, "hh:mm a"),
-            temp: d.temp,
-            icon: d.weather[0].icon,
-            humidity: d.humidity,
-            description: d.weather[0].description,
-        };
-    });
+	daily = daily.slice(1, 12).map((d) => {
+		return {
+			title: formatToLocalTime(d.dt, timezone, 'ccc'),
+			temp: d.temp.day,
+			icon: d.weather[0].icon,
+			humidity: d.humidity,
+			description: d.weather[0].description,
+		};
+	});
+	hourly = hourly.slice(1, 12).map((d) => {
+		return {
+			title: formatToLocalTime(d.dt, timezone, 'hh:mm a'),
+			temp: d.temp,
+			icon: d.weather[0].icon,
+			humidity: d.humidity,
+			description: d.weather[0].description,
+		};
+	});
 
-    return { timezone, daily, hourly };
+	return { timezone, daily, hourly };
 };
 
 const getFormattedWeatherData = async (searchParams) => {
-    const formattedCurrentWeather = await getWeatherData("weather", searchParams).then(
-        formatCurrentWeather
-    );
-    const { lat, lon } = formattedCurrentWeather;
+	const formattedCurrentWeather = await getWeatherData('weather', searchParams).then(
+		formatCurrentWeather
+	);
+	const { lat, lon } = formattedCurrentWeather;
 
-    const formattedForecastWeather = await getWeatherData("onecall", {
-        lat,
-        lon,
-        exclude: "current, minutely, alerts",
-        units: searchParams.units,
-    }).then(formatForecastWeather);
+	const formattedForecastWeather = await getWeatherData('onecall', {
+		lat,
+		lon,
+		exclude: 'current, minutely, alerts',
+		units: searchParams.units,
+	}).then(formatForecastWeather);
 
-    return { ...formattedCurrentWeather, ...formattedForecastWeather };
+	return { ...formattedCurrentWeather, ...formattedForecastWeather };
 };
 
 const formatToLocalTime = (secs, zone, format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a") =>
-    DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
+	DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
 
 const iconUrlFromCode = (code) => ` https://openweathermap.org/img/wn/${code}@2x.png`;
 
